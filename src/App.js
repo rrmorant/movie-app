c9c57cdc724700b63f7b8b3c7d15c56502de8b4e
Ron M <duecel@icloud.com>
2024-07-15 22:49:01 -0700

    chore: Add bootstrap dependency and update MovieList component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import MovieList from "./components/MovieList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [movies, setMovies] = useState([
+		{
+			Title: "Star Wars: Episode IV - A New Hope",
+			Year: "1977",
+			imdbID: "tt0076759",
+			Type: "movie",
+			Poster:
+				"https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
+		},
+		{
+			Title: "Star Wars: Episode V - The Empire Strikes Back",
+			Year: "1980",
+			imdbID: "tt0080684",
+			Type: "movie",
+			Poster:
+				"https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+		},
+		{
+			Title: "Star Wars: Episode VI - Return of the Jedi",
+			Year: "1983",
+			imdbID: "tt0086190",
+			Type: "movie",
+			Poster:
+				"https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+		},
+	]);
+	return (
+		<div>
+			<MovieList movies={movies} />
+		</div>
+	);
+};
 
 export default App;
