e3575a41ee870740f8f33c1e02d9124ace990913
Ron M <duecel@icloud.com>
2024-07-16 21:38:46 -0700

    Functionality completed, require styling

--- src/App.js
+++ src/App.js
@@ -2,28 +2,80 @@ import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
+import AddFavorites from "./components/AddFavorites";
+import RemoveFavorites from "./components/RemoveFavorites";
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
+	const [favorites, setFavorites] = useState([]);
+	const [searchValue, setSearchValue] = useState("");
 
-	const getMovieRequest = async () => {
-		const url = "http://www.omdbapi.com/?s=star wars&apikey=b21918f2";
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b21918f2`;
 
 		const response = await fetch(url);
 		const responseJson = await response.json();
 
-		console.log(responseJson);
-		setMovies(responseJson.Search);
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
 	};
 
 	useEffect(() => {
-		getMovieRequest();
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	useEffect(() => {
+		const movieFavorites = JSON.parse(
+			localStorage.getItem("react-movie-app-favorites")
+		);
+
+		setFavorites(movieFavorites);
 	}, []);
+
+	const saveToLocalStorage = (items) => {
+		localStorage.setItem("react-movie-app-favorites", JSON.stringify(items));
+	};
+
+	const addFavoriteMovie = (movie) => {
+		const newFavoriteList = [...favorites, movie];
+		setFavorites(newFavoriteList);
+		saveToLocalStorage(newFavoriteList);
+	};
+
+	const removeFavoriteMovie = (movie) => {
+		const newFavoriteList = favorites.filter(
+			(favorite) => favorite.imdbID !== movie.imdbID
+		);
+
+		setFavorites(newFavoriteList);
+		saveToLocalStorage(newFavoriteList);
+	};
 
 	return (
 		<div className="container-fluid movie-app">
+			<div className="row d-flex align-tems-center mt-4 mb-4">
+				<MovieListHeading heading="Movies" />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
 			<div className="row">
-				<MovieList movies={movies} />
+				<MovieList
+					movies={movies}
+					handleFavoritesClick={addFavoriteMovie}
+					favoriteComponent={AddFavorites}
+				/>
+			</div>
+			<div className="row d-flex align-tems-center mt-4 mb-4">
+				<MovieListHeading heading="Favorites" />
+			</div>
+			<div className="row">
+				<MovieList
+					movies={favorites}
+					handleFavoritesClick={removeFavoriteMovie}
+					favoriteComponent={RemoveFavorites}
+				/>{" "}
 			</div>
 		</div>
 	);
